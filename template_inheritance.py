8f91d64fb23fb1de07494ccbc6021b991660039d
SanjogGautam <164294030+SanjogGautam@users.noreply.github.com>
2026-05-19 13:03:03 +0545

    snajog

--- template_inheritance.py
+++ template_inheritance.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request,HTTPException,status
 from fastapi.responses import HTMLResponse 
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles  # 1. Import the static file handler
+#for exception handling
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as starletteHTTPException
 
 app = FastAPI()
 
@@ -34,7 +38,60 @@ def home(request: Request):
             "title": "Home"
         }
     )
+@app.get("/posts/{post_id}",include_in_schema=False)
+def post_page(request:Request,post_id:int):
+    for post in posts:
+        if post.get("id")==post_id:
+            return templates.TemplateResponse(
+        request,name="post.html",
+        context={
+            "post": post, 
+            "title": post['name']
+        } )
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Post is not found")
+#it is way to implement the url path
+@app.get("/api/posts/{post_id}")
+def get_posts(post_id:int):
+    for post in posts:
+        if post.get("id")==post_id:
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Post is not found")
 
 @app.get("/api/posts")
 def get_posts():
     return posts
+#general http exception handler
+@app.exception_handler(starletteHTTPException)
+def general_http_excetption_handler(request:Request,exception: starletteHTTPException):
+    message=(exception.detail
+             if exception.detail
+             else "An error Occurred!")
+    if request.url.path.startswith("/api"):
+        return JSONResponse(
+            status_code=exception.status_code,
+            context={"detail": message}
+        )
+    return templates.TemplateResponse(request, name="error.html",
+                                      context=
+                                      {
+                                       "status_code":exception.status_code,
+                                       "title": exception.status_code,
+                                       "message": message  
+                                      },
+                                      status_code=exception.status_code)
+#general request validation error
+@app.exception_handler(RequestValidationError)
+def general_http_excetption_handler(request:Request,exception: RequestValidationError):
+    if request.url.path.startswith("/api"):
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+            context={"detail": exception.errors()}
+        )
+    return templates.TemplateResponse(request, name="error.html",
+                                      context=
+                                      {
+                                       "status_code":status.HTTP_422_UNPROCESSABLE_CONTENT,
+                                       "title": status.HTTP_422_UNPROCESSABLE_CONTENT,
+                                       "message": "invalid Request! check your input and try again"
+                                      },
+                                      status_code=status.HTTP_422_UNPROCESSABLE_CONTENT)
